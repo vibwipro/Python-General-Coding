7387ccc4b9b5af811d02940a67695d62815079f5
Vibhor <49004578+vibwipro@users.noreply.github.com>
2019-09-13 23:28:38 +0200

    Code_Ed - Print_Position
    Code_Ed - Print_Position

---
+++ Common Python Code/Code_Ed/2_4_Print_Position.py
@@ -0,0 +1,12 @@
+'''
+Write a for loop that prints all elements of a list and their position in the list.
+a = [4,7,3,2,5,9]
+Hint: Use Loop to iterate through list elements.
+
+'''
+
+a = [4,7,3,2,5,9]
+p = 1
+for i in a :
+    print (str(i) + ' position in list is ' + str(p))
+    p += 1
