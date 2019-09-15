7138c82de1384a3cafb69f9b0a69a52cbe36d9bf
Vibhor <49004578+vibwipro@users.noreply.github.com>
2019-09-15 21:04:08 +0200

    Code_ED - List Intersection
    Code_ED - List Intersection

---
+++ Common Python Code/Code_Ed/2_8_List_Integration.py
@@ -0,0 +1,14 @@
+'''
+With   two   given   lists   [1,3,6,78,35,55]   and   [12,24,35,24,88,120,155],
+write   a program to make a list whose elements are intersection of the above given lists.
+
+'''
+
+a = [1,3,6,78,35,55]
+b = [12,24,35,24,88,120,155]
+
+for i in a :
+    for j in b :
+        if (i == j ) :
+            print (i)
+
